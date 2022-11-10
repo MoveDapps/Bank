44bad4f4e3c1c3a7bbffc6165fc5e42f2e9d15bb
Bidhan Roy <mail2bidhan@gmail.com>
2022-11-10 13:53:10 +0000

    make borrow public

--- sui_commands.sh
+++ sui_commands.sh
@@ -1,3 +1,4 @@
 sui client call --package {PACKAGE} --module market --function deposit_collateral --gas-budget 30000 --type-args {COIN_TYPE} --args {MARKET} {COIN}
 sui client call --package {PACKAGE} --module market --function withdraw --gas-budget 30000 --type-args {COIN_TYPE} --args {MARKET} {AMOUNT}
 
+sui client call --package {PACKAGE} --module market --function borrow --gas-budget 30000 --type-args {BORROW_TYPE} {COL_TYPE} --args {BOR} {COL} {MARKET}
